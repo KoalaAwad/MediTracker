0318ab2a55536875f5170fc98a6ea3739a13cddc
Awad <Hew7536@gmail.com>
2025-09-02 11:57:24 +0400

    first commit

---
+++ src/main/java/com/example/meditracker/MediTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.meditracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediTrackerApplication.class, args);
+	}
+
+}
