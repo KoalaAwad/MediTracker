99440036e1415299e940ae24c0245addb2300bc1
Awad <Hew7536@gmail.com>
2025-10-29 09:47:47 +0400

    error handling and error pages

--- src/main/java/org/springbozo/meditracker/controller/GlobalExceptionHandler.java
+++ src/main/java/org/springbozo/meditracker/controller/GlobalExceptionHandler.java
@@ -1,16 +1,28 @@
 package org.springbozo.meditracker.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalDefaultExceptionHandler {
+    public static final String DEFAULT_ERROR_VIEW = "error";
 
-    @ExceptionHandler(Exception.class)
-    public String handleAllExceptions(Exception ex, Model model) {
-        model.addAttribute("errorMessage", ex.getMessage());
-        return "public/error-page";
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        // If the exception has a specific HTTP status, rethrow it
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null)
+            throw e;
+
+        // Otherwise, render your custom error page
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName(DEFAULT_ERROR_VIEW);
+        return mav;
     }
 }
-
