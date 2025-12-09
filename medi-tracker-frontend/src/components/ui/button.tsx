1a481361df9d143a22874633270b571c1771a5af
Awad <Hew7536@gmail.com>
2025-12-09 21:24:10 +0400

    syncing with pc

--- medi-tracker-frontend/src/components/ui/button.tsx
+++ medi-tracker-frontend/src/components/ui/button.tsx
@@ -44,6 +44,7 @@ function Button({
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
+
     asChild?: boolean
   }) {
   const Comp = asChild ? Slot : "button"
